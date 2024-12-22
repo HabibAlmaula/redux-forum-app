165e4aaf87616ffd59497cb42b01b9c80b29621a
habibalmaula <habibalmaula@gmail.com>
2024-12-22 11:05:03 +0700

    fix : vite config for cypress

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Make sure this matches the port Cypress is trying to access
+    host: true   // This allows external connections
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
