500f842be3665dc1f1a8eb981b0a24d3a6c41b17
habibalmaula <habibalmaula@gmail.com>
2024-12-22 21:22:25 +0700

    feat : add story book

--- src/components/app/ThreadList.jsx
+++ src/components/app/ThreadList.jsx
@@ -1,12 +1,15 @@
 import { PropTypes } from 'prop-types';
 import { ThreadCard, threadItemShape } from './ThreadCard';
+import { useNavigate } from 'react-router';
 const ThreadList = ({ threads }) => {
+  const navigate = useNavigate();
   return (
     <div className="thread-list mt-10 md:mt-0">
       {threads.map((thread) => (
         <ThreadCard
           key={thread.id}
           {...thread}
+          onClick={() => navigate(`/detail-thread/${thread.id}`)}
         />
       ))}
     </div>
